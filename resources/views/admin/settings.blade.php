ac96b5f6fbfac86738590d15652f5eb2e782b4c2
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-04-27 16:00:05 +0530

    Place form actions on full-width rows and center under fields

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -81,8 +81,7 @@
                             </div>
                             <!-- Save Button -->
                             <div class="row p-1 m-0">
-                                <div class="col-6"></div>
-                                <div class="col-6 text-end">
+                                <div class="col-12 adwiseri-form-actions">
                                     <button type="button" id="save-general-settings"class="btn btn-primary">Save</button>
                                 </div>
                             </div>
@@ -120,10 +119,7 @@
                         </div>
 
                         <div class="row p-1 m-0">
-                            <div class="col-6">
-                                {{-- <label>Description</label> --}}
-                            </div>
-                            <div class="col-6 text-end">
+                            <div class="col-12 adwiseri-form-actions">
                                 <button type="button" class="btn btn-primary" id="save-invoice-settings">Save</button>
                             </div>
                         </div>
@@ -240,7 +236,7 @@
                             </div>
                         </div>
                         <div class="row p-1 m-0">
-                            <div class="col-12 text-start">
+                            <div class="col-12 text-start adwiseri-form-actions">
                                 <button type="submit" class="btn btn-primary" id="save-offers-settings">Add New Service</button>
                             </div>
                         </div>
@@ -323,8 +319,7 @@
                     </div>
 
                     <div class="row p-1 m-0">
-                        <div class="col-6"></div>
-                        <div class="col-6 text-end">
+                        <div class="col-12 adwiseri-form-actions">
                             <button type="button" class="btn btn-primary" id="save-reports-settings">Apply</button>
                             <button type="reset" class="btn btn-secondary">Cancel</button>
                         </div>
@@ -361,7 +356,7 @@
                             <div class="col-6"><textarea id="emailTemplateBody" name="body" class="form-control" rows="8"></textarea></div>
                         </div>
                         <div class="row p-1 m-0">
-                            <div class="col-12 text-end">
+                            <div class="col-12 text-end adwiseri-form-actions">
                                 <button type="button" class="btn btn-primary" id="save-email-template">Save</button>
                                 <button type="button" class="btn btn-outline-secondary" id="reset-email-template">Reset</button>
                             </div>
