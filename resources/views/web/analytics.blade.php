fde50db8855bd5aac15417fce520f56ab27b38a6
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-05-06 23:00:10 +0530

    Reorder sub analytics chart type options

--- resources/views/web/analytics.blade.php
+++ resources/views/web/analytics.blade.php
@@ -166,8 +166,8 @@
     <label class="fw-bold" >Select Chart Type</label>
     <select id="chartType" class="form-select" aria-label="Default select example">
         <option value="" selected>Select Chart Type</option>
-        <option value="bar">Bar</option>
         <option value="line">Line</option>
+        <option value="bar">Bar</option>
         <option value="pie">Pie</option>
         <option value="doughnut">Doughnut</option>
     </select>
