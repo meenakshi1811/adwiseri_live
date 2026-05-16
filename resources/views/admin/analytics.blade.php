a5e4f3a4a489178f9005087394559fcae736baba
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-05-16 23:58:51 +0530

    Add Application Status filter to web sub_analytics view

--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -640,6 +640,10 @@ window.Chart = AnalyticsChart;
                         value: "By Application Type"
                     },
                     {
+                        text: "By Application Status",
+                        value: "By Application Status"
+                    },
+                    {
                         text: "By Application Counts By No. of Dependants",
                         value: "ByApplicationCountsByDependants"
                     },
@@ -5124,6 +5128,142 @@ window.Chart = AnalyticsChart;
                             downloadButton.removeAttribute('data-downloading'); // Reset flag
                             downloadButton.disabled = false;
                         }, 1000); // Small delay to ensure smooth UX
+                    });
+                });
+            } else if (selectedFilter == "By Application Status") {
+                let chartStatus = Chart.getChart("myChart"); // <canvas> id
+                if (chartStatus != undefined) {
+                    chartStatus.destroy();
+                }
+                $.ajax({
+                    type: 'GET',
+                    url: "{{ route('subscribersReport') }}",
+
+                    data: {
+                        type: 'byApplicationStatus',
+                        subid: subID,
+                        startDate: startDate,
+                        endDate: endDate
+                    },
+                    success: function(data) {
+                        if (data.data.length === 0) {
+                            alert('No data found');
+                            return;
+                        }
+
+                        var result = data.data;
+                        var labels = [];
+                        var numbers = [];
+
+                        result.forEach(function(currentElement) {
+                            if (currentElement.status_count !== 0) {
+                                labels.push(currentElement.application_status);
+                                numbers.push(currentElement.status_count);
+                            }
+                        });
+
+                        const ctx = document.getElementById('myChart');
+                        const dynamicColors = generateDistinctColors(labels.length);
+
+                        new Chart(ctx, {
+                            type: chartType,
+                            data: {
+                                labels: labels,
+                                datasets: [{
+                                    label: selectedAttribute + ' ' + selectedFilter,
+                                    data: numbers,
+                                    borderWidth: 1,
+                                    backgroundColor: dynamicColors,
+                                }]
+                            },
+                            options: {
+                                responsive: false,
+                                scales: {
+                                    y: {
+                                        beginAtZero: true,
+                                        ticks: {
+                                            stepSize: 1,
+                                            precision: 0
+                                        }
+                                    }
+                                },
+                                plugins: {
+                                    title: {
+                                        display: true,
+                                        text: title,
+                                        font: {
+                                            size: 20,
+                                            weight: 800
+                                        },
+                                        padding: {
+                                            bottom: 50
+                                        },
+                                        color: 'black',
+                                        align: 'center'
+                                    },
+                                    legend: {
+                                        display: true,
+                                        position: 'bottom',
+                                        labels: {
+                                            padding: 30
+                                        }
+                                    },
+                                    tooltip: {
+                                        callbacks: {
+                                            label: function(tooltipItem) {
+                                                const dataValue = tooltipItem.raw || '';
+                                                return `No. Of Applications: ${dataValue}`;
+                                            },
+                                            afterBody: function(tooltipItem) {
+                                                const dataValue = tooltipItem[0].raw || '';
+                                                const total = tooltipItem[0].dataset.data.reduce((acc, val) => acc + val, 0);
+                                                const percentage = ((dataValue / total) * 100).toFixed(1);
+                                                return ['Percent Value: ' + percentage + '%'];
+                                            }
+                                        }
+                                    },
+                                    datalabels: {
+                                        anchor: 'end',
+                                        align: 'top',
+                                        formatter: (value) => value,
+                                        font: {
+                                            weight: 'bold'
+                                        },
+                                        color: 'black'
+                                    }
+                                }
+                            },
+                            plugins: [ChartDataLabels]
+                        });
+                    },
+                    error: function(xhr, status, error) {
+                        console.error("Error occurred: " + status + " - " + error);
+                    }
+                });
+                document.getElementById('downloadPdf').addEventListener('click', function(event) {
+                    event.preventDefault();
+                    let downloadButton = this;
+                    if (downloadButton.getAttribute('data-downloading') === 'true') {
+                        return;
+                    }
+                    downloadButton.setAttribute('data-downloading', 'true');
+                    downloadButton.disabled = true;
+                    html2canvas(document.getElementById('myChart')).then(canvas => {
+                        const imgData = canvas.toDataURL('image/png');
+                        const { jsPDF } = window.jspdf;
+                        const pdf = new jsPDF({ orientation: 'portrait', unit: 'px', format: 'a4' });
+                        const title = $('#selectAttribute').val() + " " + $('#filters').val() + " (" + dateForTitle + ")";
+                        pdf.setFontSize(16);
+                        pdf.text(title, 20, 30);
+                        pdf.addImage(imgData, 'PNG', 10, 50, 410, 410);
+                        pdf.save(title + '.pdf');
+                    }).catch(error => {
+                        console.error("Error generating PDF: ", error);
+                    }).finally(() => {
+                        setTimeout(() => {
+                            downloadButton.removeAttribute('data-downloading');
+                            downloadButton.disabled = false;
+                        }, 1000);
                     });
                 });
             } else if (selectedFilter == "By Application Type") {
